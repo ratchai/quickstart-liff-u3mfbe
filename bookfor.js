cbb86b393cf968cabdefc287bbd7917c3fad7b3e
ratchai <ratchai@gmail.com>
2021-04-19 01:32:21 +0700

    2 files modified, 2 added, 2 deleted

--- bookfor.js
+++
@@ -1,4 +0,0 @@
-const body = document.getElementById("testtext");
-console.log("query string =" + window.location.search);
-var urlParams = new URLSearchParams(window.location.search);
-const action = urlParams.get("purpose");

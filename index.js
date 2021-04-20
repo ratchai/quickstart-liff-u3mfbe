6bda1af8c2c21ad5429d38fa6548ec427e4c7b2b
ratchai <ratchai@gmail.com>
2021-04-21 00:51:52 +0700

    test

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 //import "./style.css";
 
 async function main() {
-  await liff.init({ liffId: "1655863402-51ngLPwJ" });
-  const querystring = decodeURIComponent(window.location.search);
-  const action = urlParams.get("purpose");
-  console.log(action);
+//  await liff.init({ liffId: "1655863402-51ngLPwJ" });
+ // const querystring = decodeURIComponent(window.location.search);
+//  const action = urlParams.get("purpose");
+  console.log("xxxxxxxxxxxxxxxxxx");
   // liff.openWIndow(action + ".html");
 }
 main();
